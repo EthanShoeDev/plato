6a95171152c6d7bc13cdb8130f846a6830b8a18f
EthanShoeDev <13422990+EthanShoeDev@users.noreply.github.com>
2024-01-14 06:25:42 -0500

    fix(dx): fix type checking in mdx files

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-import { NextConfig } from 'next';
 import withBundleAnalyzer from '@next/bundle-analyzer';
 import createMDX from '@next/mdx';
 import remarkGfm from 'remark-gfm';
 
 const nextConfig = {
-  // Configure `pageExtensions`` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
 };
 
 const withMDX = createMDX({
@@ -17,9 +14,9 @@ const withMDX = createMDX({
   },
 });
 
-// const bundleAnalyzer = withBundleAnalyzer({
-//   enabled: process.env.ANALYZE === 'true',
-// });
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default bundleAnalyzer(withMDX(nextConfig));
